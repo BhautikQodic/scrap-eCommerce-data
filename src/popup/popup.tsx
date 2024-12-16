312e6b136f16bc4105ac7daf7ab4ef7956ec0d09
Bhautik Gajera <bhautik@qodictechnosoft.com>
2024-12-16 11:02:21 +0530

    initial project setup

---
+++ src/popup/popup.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import '../assets/tailwind.css';
+import { App } from "..";
+
+const container = document.createElement("div");
+document.body.appendChild(container);
+
+const root = createRoot(container)
+root.render(<App />);
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const autoprefixer = require("autoprefixer");
+const CopyPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const tailwindcss = require("tailwindcss");
+
+module.exports = {
+    mode: "development",
+    devtool: "cheap-module-source-map",
+    entry: {
+        // popup: path.resolve("./src/popup/popup.tsx")
+    },
+    module: {
+        rules: [
+            {
+                use: "ts-loader",
+                test: /\.tsx?$/,
+                exclude: /node_modules/
+            },
+            {
+                use: ["style-loader", "css-loader", {
+                    loader: "postcss-loader",
+                    options: {
+                        postcssOptions: {
+                            ident: "postcss",
+                            plugins: [
+                                tailwindcss, autoprefixer
+                            ]
+                        }
+                    }
+                }],
+                test: /\.css$/i,
+            }
+        ]
+    },
+    plugins: [ 
+        new CopyPlugin({
+            patterns: [
+                { from: path.resolve("./src/static"), to: path.resolve("dist") },
+            ],
+        }),
+        new HtmlWebpackPlugin({
+            title: "Password Generator",
+            filename: "popup.html",
+            chunks: ["popup"]
+        })
+    ],
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"]
+    },
+    output: {
+        filename: '[name].js',
+    }
+}
